7b786a88b624c7324bb30b1a42b706b8aa30dc42
Kim Järvinen <Kim.Jarvinen@student.lab.fi>
2020-12-27 21:14:07 +0200

    init (CONFIGISSA OMAT ARVOT!!!)

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Bitcoin_Sovellus
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.bitcoinLogo = new System.Windows.Forms.PictureBox();
+            this.tekstiLaatikko = new System.Windows.Forms.TextBox();
+            this.ostettuNappula = new System.Windows.Forms.Button();
+            this.myytyNappula = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dollarLabel = new System.Windows.Forms.Label();
+            this.myyntiSijaintiLabel = new System.Windows.Forms.Label();
+            this.tiedostonSijaintiLabel = new System.Windows.Forms.Label();
+            this.vaihdaSijaintiButton = new System.Windows.Forms.Button();
+            this.paivaMaaraUpLabel = new System.Windows.Forms.Label();
+            this.paivaMaaraDownLabel = new System.Windows.Forms.Label();
+            this.kaytitViimeksiUpLabel = new System.Windows.Forms.Label();
+            this.kaytitViimeksiDownLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bitcoinLogo)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bitcoinLogo
+            // 
+            this.bitcoinLogo.Image = ((System.Drawing.Image)(resources.GetObject("bitcoinLogo.Image")));
+            this.bitcoinLogo.Location = new System.Drawing.Point(31, 34);
+            this.bitcoinLogo.Name = "bitcoinLogo";
+            this.bitcoinLogo.Size = new System.Drawing.Size(85, 77);
+            this.bitcoinLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bitcoinLogo.TabIndex = 0;
+            this.bitcoinLogo.TabStop = false;
+            // 
+            // tekstiLaatikko
+            // 
+            this.tekstiLaatikko.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tekstiLaatikko.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tekstiLaatikko.Location = new System.Drawing.Point(9, 7);
+            this.tekstiLaatikko.MaxLength = 12;
+            this.tekstiLaatikko.Name = "tekstiLaatikko";
+            this.tekstiLaatikko.Size = new System.Drawing.Size(132, 19);
+            this.tekstiLaatikko.TabIndex = 1;
+            this.tekstiLaatikko.TabStop = false;
+            this.tekstiLaatikko.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tekstiLaatikko_KeyPress);
+            // 
+            // ostettuNappula
+            // 
+            this.ostettuNappula.BackColor = System.Drawing.SystemColors.Window;
+            this.ostettuNappula.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ostettuNappula.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(147)))), ((int)(((byte)(26)))));
+            this.ostettuNappula.FlatAppearance.BorderSize = 2;
+            this.ostettuNappula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ostettuNappula.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ostettuNappula.Location = new System.Drawing.Point(30, 168);
+            this.ostettuNappula.Name = "ostettuNappula";
+            this.ostettuNappula.Size = new System.Drawing.Size(83, 31);
+            this.ostettuNappula.TabIndex = 2;
+            this.ostettuNappula.TabStop = false;
+            this.ostettuNappula.Text = "Osto";
+            this.ostettuNappula.UseVisualStyleBackColor = false;
+            this.ostettuNappula.Click += new System.EventHandler(this.ostettuNappula_Click);
+            // 
+            // myytyNappula
+            // 
+            this.myytyNappula.BackColor = System.Drawing.SystemColors.Window;
+            this.myytyNappula.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.myytyNappula.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(147)))), ((int)(((byte)(26)))));
+            this.myytyNappula.FlatAppearance.BorderSize = 2;
+            this.myytyNappula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myytyNappula.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myytyNappula.Location = new System.Drawing.Point(120, 168);
+            this.myytyNappula.Name = "myytyNappula";
+            this.myytyNappula.Size = new System.Drawing.Size(83, 31);
+            this.myytyNappula.TabIndex = 3;
+            this.myytyNappula.TabStop = false;
+            this.myytyNappula.Text = "Myynti";
+            this.myytyNappula.UseVisualStyleBackColor = false;
+            this.myytyNappula.Click += new System.EventHandler(this.myytyNappula_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(147)))), ((int)(((byte)(26)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(30, 126);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 36);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.dollarLabel);
+            this.panel2.Controls.Add(this.tekstiLaatikko);
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(169, 32);
+            this.panel2.TabIndex = 5;
+            // 
+            // dollarLabel
+            // 
+            this.dollarLabel.AutoSize = true;
+            this.dollarLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dollarLabel.Location = new System.Drawing.Point(147, 7);
+            this.dollarLabel.Name = "dollarLabel";
+            this.dollarLabel.Size = new System.Drawing.Size(18, 20);
+            this.dollarLabel.TabIndex = 5;
+            this.dollarLabel.Text = "€";
+            // 
+            // myyntiSijaintiLabel
+            // 
+            this.myyntiSijaintiLabel.AutoSize = true;
+            this.myyntiSijaintiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myyntiSijaintiLabel.Location = new System.Drawing.Point(28, 266);
+            this.myyntiSijaintiLabel.Name = "myyntiSijaintiLabel";
+            this.myyntiSijaintiLabel.Size = new System.Drawing.Size(232, 16);
+            this.myyntiSijaintiLabel.TabIndex = 5;
+            this.myyntiSijaintiLabel.Text = "Tiedosto myynti- ja ostotietoja varten: ";
+            // 
+            // tiedostonSijaintiLabel
+            // 
+            this.tiedostonSijaintiLabel.AutoSize = true;
+            this.tiedostonSijaintiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tiedostonSijaintiLabel.Location = new System.Drawing.Point(28, 287);
+            this.tiedostonSijaintiLabel.Name = "tiedostonSijaintiLabel";
+            this.tiedostonSijaintiLabel.Size = new System.Drawing.Size(111, 16);
+            this.tiedostonSijaintiLabel.TabIndex = 6;
+            this.tiedostonSijaintiLabel.Text = "(tiedoston sijainti)";
+            // 
+            // vaihdaSijaintiButton
+            // 
+            this.vaihdaSijaintiButton.Location = new System.Drawing.Point(31, 313);
+            this.vaihdaSijaintiButton.Name = "vaihdaSijaintiButton";
+            this.vaihdaSijaintiButton.Size = new System.Drawing.Size(109, 23);
+            this.vaihdaSijaintiButton.TabIndex = 7;
+            this.vaihdaSijaintiButton.TabStop = false;
+            this.vaihdaSijaintiButton.Text = "Vaihda tiedosto?";
+            this.vaihdaSijaintiButton.UseVisualStyleBackColor = true;
+            this.vaihdaSijaintiButton.Click += new System.EventHandler(this.vaihdaSijaintiButton_Click);
+            // 
+            // paivaMaaraUpLabel
+            // 
+            this.paivaMaaraUpLabel.AutoSize = true;
+            this.paivaMaaraUpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paivaMaaraUpLabel.Location = new System.Drawing.Point(337, 61);
+            this.paivaMaaraUpLabel.Name = "paivaMaaraUpLabel";
+            this.paivaMaaraUpLabel.Size = new System.Drawing.Size(105, 16);
+            this.paivaMaaraUpLabel.TabIndex = 8;
+            this.paivaMaaraUpLabel.Text = "Päivämäärä nyt:";
+            // 
+            // paivaMaaraDownLabel
+            // 
+            this.paivaMaaraDownLabel.AutoSize = true;
+            this.paivaMaaraDownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paivaMaaraDownLabel.Location = new System.Drawing.Point(337, 80);
+            this.paivaMaaraDownLabel.Name = "paivaMaaraDownLabel";
+            this.paivaMaaraDownLabel.Size = new System.Drawing.Size(89, 16);
+            this.paivaMaaraDownLabel.TabIndex = 9;
+            this.paivaMaaraDownLabel.Text = "(päivämäärä)";
+            // 
+            // kaytitViimeksiUpLabel
+            // 
+            this.kaytitViimeksiUpLabel.AutoSize = true;
+            this.kaytitViimeksiUpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kaytitViimeksiUpLabel.Location = new System.Drawing.Point(337, 120);
+            this.kaytitViimeksiUpLabel.Name = "kaytitViimeksiUpLabel";
+            this.kaytitViimeksiUpLabel.Size = new System.Drawing.Size(159, 16);
+            this.kaytitViimeksiUpLabel.TabIndex = 10;
+            this.kaytitViimeksiUpLabel.Text = "Käytit sovellusta viimeksi:";
+            // 
+            // kaytitViimeksiDownLabel
+            // 
+            this.kaytitViimeksiDownLabel.AutoSize = true;
+            this.kaytitViimeksiDownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kaytitViimeksiDownLabel.Location = new System.Drawing.Point(337, 139);
+            this.kaytitViimeksiDownLabel.Name = "kaytitViimeksiDownLabel";
+            this.kaytitViimeksiDownLabel.Size = new System.Drawing.Size(89, 16);
+            this.kaytitViimeksiDownLabel.TabIndex = 11;
+            this.kaytitViimeksiDownLabel.Text = "(päivämäärä)";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 385);
+            this.Controls.Add(this.kaytitViimeksiDownLabel);
+            this.Controls.Add(this.kaytitViimeksiUpLabel);
+            this.Controls.Add(this.paivaMaaraDownLabel);
+            this.Controls.Add(this.paivaMaaraUpLabel);
+            this.Controls.Add(this.vaihdaSijaintiButton);
+            this.Controls.Add(this.tiedostonSijaintiLabel);
+            this.Controls.Add(this.myyntiSijaintiLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.myytyNappula);
+            this.Controls.Add(this.ostettuNappula);
+            this.Controls.Add(this.bitcoinLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bitcoin Tallennin";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bitcoinLogo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bitcoinLogo;
+        private System.Windows.Forms.TextBox tekstiLaatikko;
+        private System.Windows.Forms.Button ostettuNappula;
+        private System.Windows.Forms.Button myytyNappula;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label dollarLabel;
+        private System.Windows.Forms.Label myyntiSijaintiLabel;
+        private System.Windows.Forms.Label tiedostonSijaintiLabel;
+        private System.Windows.Forms.Button vaihdaSijaintiButton;
+        private System.Windows.Forms.Label paivaMaaraUpLabel;
+        private System.Windows.Forms.Label paivaMaaraDownLabel;
+        private System.Windows.Forms.Label kaytitViimeksiUpLabel;
+        private System.Windows.Forms.Label kaytitViimeksiDownLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bitcoin_Sovellus
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        string formMainFolder = AppDomain.CurrentDomain.BaseDirectory; // Sovelluksen pääkansio
+        string formConfig; // Sovelluksen config-tiedosto
+
+        string directoryPath; // Mahdollinen vaihdettu tiedoston sijainti (ks. vaihdaSijaintiButton)
+        string tiedostonSijainti; // Tiedoston virallinen sijainti (oletus: "D:\stuff\fileread\bitcoin.txt")
+
+        // CONFIG INIT:
+        /*
+            directoryPath = null
+            dateTime = null
+        */
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            formConfig = formMainFolder + @"\config.txt";
+
+            List<string> lines = new List<string>(); // Uuden listan luominen
+            lines = File.ReadAllLines(formConfig).ToList(); // Lue koko tiedosto ja lisää se listaan
+
+            // Lue directory path
+            if (lines[0] == "directoryPath = null")
+            {
+                directoryPath = null;
+                tiedostonSijainti = @"D:\stuff\fileread\bitcoin.txt"; // Myynti- ja ostotietojen normaali tallennussijainti
+            }
+            else if (lines[0].Contains("directoryPath ="))
+            {
+                string lines0 = lines[0];
+                directoryPath = lines0.Substring(16);
+                tiedostonSijainti = directoryPath;
+            }
+
+            tiedostonSijaintiLabel.Text = tiedostonSijainti;
+
+            // Lue date time
+            if (lines[1] == "dateTime = null")
+            {
+                kaytitViimeksiDownLabel.Text = "";
+            }
+            else if (lines[1].Contains("dateTime ="))
+            {
+                string lines1 = lines[1];
+                kaytitViimeksiDownLabel.Text = lines1.Substring(11);
+            }
+
+            // Ilmoita päivämäärä
+            DateTime date = DateTime.Now;
+            paivaMaaraDownLabel.Text = date.ToString("dd/MM/yyyy");
+        }
+
+        private void ostettuNappula_Click(object sender, EventArgs e)
+        {
+            DialogResult varmistus = MessageBox.Show("Oletko varma, että OSTAT bitcoineja?", "Ostatko?", MessageBoxButtons.YesNo);
+
+            if (varmistus == DialogResult.Yes)
+            {
+            string teksti = tekstiLaatikko.Text; // Lue tekstilaatikon teksti ja lisää se muuttujaan
+
+            string fileLocation = tiedostonSijainti; // Tallennettavan tiedoston sijainti
+
+            DateTime date = DateTime.Now; // Määritä tämänhetkinen aika
+
+            List<string> lines = new List<string>(); // Uuden listan luominen
+            lines = File.ReadAllLines(fileLocation).ToList(); // Lue koko tiedosto ja lisää se listaan
+
+            lines.Add("Ostettu [" + date.ToString("dd/MM/yyyy") + "]: " + teksti + "€\n"); // Lisää listaan "Ostettu [_AIKA_]: _EUROMÄÄRÄ_€"
+            File.WriteAllLines(fileLocation, lines); // Päivitä tiedosto listan sisällöllä
+
+                // Päivitä config-tiedostoon aika, jolloin tallennus tehtiin
+                formConfig = formMainFolder + @"\config.txt";
+
+                List<string> lines2 = new List<string>(); // Uuden listan luominen
+                lines2 = File.ReadAllLines(formConfig).ToList(); // Lue koko tiedosto ja lisää se listaan
+
+                lines2[1] = "dateTime = " + date.ToString("dd/MM/yyyy");
+                File.WriteAllLines(formConfig, lines2); // Päivitä tiedosto listan sisällöllä
+            }
+        }
+
+        private void myytyNappula_Click(object sender, EventArgs e)
+        {
+            DialogResult varmistus = MessageBox.Show("Oletko varma, että MYYT bitcoineja?", "Myytkö?", MessageBoxButtons.YesNo);
+
+            if (varmistus == DialogResult.Yes)
+            {
+                string teksti = tekstiLaatikko.Text; // Lue tekstilaatikon teksti ja lisää se muuttujaan
+
+                string fileLocation = tiedostonSijainti; // Tallennettavan tiedoston sijainti
+
+                DateTime date = DateTime.Now; // Määritä tämänhetkinen aika
+
+                List<string> lines = new List<string>(); // Uuden listan luominen
+                lines = File.ReadAllLines(fileLocation).ToList(); // Lue koko tiedosto ja lisää se listaan
+
+                lines.Add("Myyty [" + date.ToString("dd/MM/yyyy") + "]: " + teksti + "€\n"); // Lisää listaan "Myyty [_AIKA_]: _EUROMÄÄRÄ_€"
+                File.WriteAllLines(fileLocation, lines); // Päivitä tiedosto listan sisällöllä
+
+                    // Päivitä config-tiedostoon aika, jolloin tallennus tehtiin
+                    formConfig = formMainFolder + @"\config.txt";
+
+                    List<string> lines2 = new List<string>(); // Uuden listan luominen
+                    lines2 = File.ReadAllLines(formConfig).ToList(); // Lue koko tiedosto ja lisää se listaan
+
+                    lines2[1] = "dateTime = " + date.ToString("dd/MM/yyyy");
+                    File.WriteAllLines(formConfig, lines2); // Päivitä tiedosto listan sisällöllä
+            }
+        }
+
+        private void tekstiLaatikko_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void vaihdaSijaintiButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.FileName = "Bitcoin-tapahtumani.txt";
+            fileDialog.Filter = "Text files (*.txt)|*.txt";
+            fileDialog.RestoreDirectory = true;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                directoryPath = fileDialog.InitialDirectory + fileDialog.FileName;
+
+                if (!File.Exists(fileDialog.FileName)) // Jos tiedostoa ei ole olemassa
+                {
+                    File.Create(fileDialog.FileName).Close(); // Luo uusi tiedosto
+                }
+                /* else // Jos tiedosto on olemassa
+                {
+                    // Tee jotain
+                } */
+
+                // Päivitä config-tiedosto
+                DateTime date = DateTime.Now;
+
+                formConfig = formMainFolder + @"\config.txt";
+
+                List<string> lines = new List<string>(); // Uuden listan luominen
+                lines = File.ReadAllLines(formConfig).ToList(); // Lue koko tiedosto ja lisää se listaan
+
+                lines[0] = "directoryPath = " + directoryPath;
+                lines[1] = "dateTime = " + date.ToString("dd/MM/yyyy");
+                File.WriteAllLines(formConfig, lines); // Päivitä tiedosto listan sisällöllä
+            }
+
+            // Päivitä tiedoston sijainti -teksti
+            if (directoryPath != null)
+            {
+                tiedostonSijainti = directoryPath; // Myynti- ja ostotietojen uusi tallennussijainti
+            }
+            else
+            {
+                tiedostonSijainti = @"D:\stuff\fileread\bitcoin.txt"; // Myynti- ja ostotietojen normaali tallennussijainti
+            }
+
+            tiedostonSijaintiLabel.Text = tiedostonSijainti;
+        }
+    }
+}
